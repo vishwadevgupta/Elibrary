de56985ea9f982eb5dbbe22d5d8624598e4851d8
Vishwadev Gupta <75661083+vishwadevgupta@users.noreply.github.com>
2026-09-24 01:11:20 +0530

    Secure librarian administration and actions

--- src/com/javatpoint/servlets/DeleteBook.java
+++ src/com/javatpoint/servlets/DeleteBook.java
@@ -1,18 +1,3 @@
 package com.javatpoint.servlets;
-
-
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import com.javatpoint.dao.BookDao;
-@WebServlet("/DeleteBook")
-public class DeleteBook extends HttpServlet {
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		BookDao.delete(request.getParameter("callno"));
-		response.sendRedirect("ViewBook");
-	}
-}
+import java.io.IOException;import javax.servlet.*;import javax.servlet.annotation.*;import javax.servlet.http.*;import com.javatpoint.dao.BookDao;import com.javatpoint.util.AuthUtil;
+@WebServlet("/DeleteBook") public class DeleteBook extends HttpServlet{protected void doPost(HttpServletRequest q,HttpServletResponse r)throws ServletException,IOException{if(!AuthUtil.requireLibrarian(q,r))return;BookDao.delete(q.getParameter("callno"));r.sendRedirect("ViewBook");}}
